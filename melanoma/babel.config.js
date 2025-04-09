61d304ca5bbe786089e6d62147756146542653f4
NashIvan-cmd <nashivanramos@gmail.com>
2025-04-10 01:02:03 +0800

    Added API_URL Prefix globally

--- melanoma/babel.config.js
+++ melanoma/babel.config.js
@@ -1,18 +1,28 @@
-module.exports = function(api) {
+module.exports = function (api) {
     api.cache(true);
-
+  
     return {
-        presets: [["babel-preset-expo", {
-            jsxImportSource: "nativewind"
-        }], "nativewind/babel"],
-
-        plugins: [["module-resolver", {
-            root: ["./"],
-
-            alias: {
-                "@": "./",
-                "tailwind.config": "./tailwind.config.js"
-            }
-        }]]
+      presets: [
+        ["babel-preset-expo", {
+          jsxImportSource: "nativewind",
+        }],
+        "nativewind/babel",
+      ],
+      plugins: [
+        ["module-resolver", {
+          root: ["./"],
+          alias: {
+            "@": "./",
+            "tailwind.config": "./tailwind.config.js",
+          },
+        }],
+        ["dotenv-import", {
+          moduleName: "@env",
+          path: ".env",
+          safe: false,
+          allowUndefined: true,
+        }],
+      ],
     };
-};
+  };
+  
