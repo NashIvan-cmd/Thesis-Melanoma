4ceb8c3c39159d9298e2f809e984099e0879bfa1
NashIvan-cmd <nashivanramos@gmail.com>
2025-04-27 00:32:00 +0800

    focusEffect and mole details fetch

--- melanoma/app/(app)/(tabs)/(photo)/index.tsx
+++ melanoma/app/(app)/(tabs)/(photo)/index.tsx
@@ -1,8 +1,9 @@
 import { View, Text, ScrollView, Linking, Alert, StyleSheet, FlatList } from 'react-native'
+import { useFocusEffect } from 'expo-router';
 import { Image } from 'expo-image';
 import { Link, useRouter } from 'expo-router';
 //import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import React, { useState, useCallback, useRef } from 'react'
+import React, { useState, useCallback, useRef, useEffect } from 'react'
 //import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 //import * as Device from 'expo-device';
@@ -16,13 +17,59 @@ import { Pressable } from '@/components/ui/pressable';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
+import { useSession } from '@/services/authContext';
+import { molesToDisplay } from '@/api/moleData';
+
+type Mole = {
+    id: string;
+    body_orientation: string;
+    body_part: string;
+    mole_owner: string;
+    x_coordinate: number;
+    y_coordinate: number;
+    cloudId: string;
+    publicId: string;
+    createdAt: string;
+    signedUrl: string;
+  };
+
 const Photo = () => {
+    const { userId, accessToken } = useSession();
     const [facing, setFacing] = useState<CameraType>('back');
     const [permission, requestPermission] = useCameraPermissions();
-    const [uri, setUri] = useState<string | null>();;
+    const [uri, setUri] = useState<string | null>();
+    const [molesArray, setMolesArray] = useState([]);
     const ref = useRef<CameraView>(null);
 
     const router = useRouter();
+
+    useFocusEffect(
+        useCallback(() => {
+            console.log("Use effect running");
+            const runAsyncEffect = async() => {
+                try {
+                if (!userId) throw new Error("Missing userId");
+                if (!accessToken) throw new Error("Missing access token");
+                
+                const response = await molesToDisplay(userId, accessToken);
+                
+                const result = await response?.json();
+                
+                console.log({ result });
+                if(!result) throw new Error("No response from molesToDisplay");
+
+                setMolesArray(result.manipulatedMoles);
+                } catch (error) {
+                console.error("Error @ run async effect", error);
+                }    
+            }
+
+            runAsyncEffect();
+            console.log('Moles array', molesArray);
+        }, [userId])
+    );
+
+    
 
     const handleOpenSettings = () => {
         Linking.openSettings().catch(() => {
@@ -101,22 +148,22 @@ const Photo = () => {
         <View className='flex-1 bg-custom-dark p-[20]'>
             <Text>Of the Goodness of God</Text>
             <View>
-                <FlatList
-                    data={Array(10).fill(null)}  // Replace with actual data
+                <FlatList<Mole>
+                    data={molesArray}  // Replace with actual data
                     keyExtractor={(_, index) => index.toString()}
                     showsVerticalScrollIndicator={false}
                     renderItem={({ item, index }) => (
                         <View key={index} className='flex flex-row h-[90] w-full bg-slate-600/80 p-[10] mt-[5] rounded'>
                             <View className="min-h-[40px] min-w-[40px] bg-orange-500 mr-[10] rounded">
-                                <Text>Picture</Text>
+                                <Image source={{ uri: item.cloudId }}  style={{ width: 40, height: 70, borderRadius: 8 }}/>
                             </View>
                             <View>
                                 <Text>{" "}</Text>
                             </View>
                             <View className='ml-[10]'>
-                                <Text className="text-slate-400 text-md">Body Part</Text>
-                                <Text className='text-slate-400 text-md'>Info</Text>
-                                <Text className='text-slate-400 text-md'>Last Checked</Text>
+                                <Text className="text-slate-400 text-md">{item.body_part}</Text>
+                                <Text className='text-slate-400 text-md'></Text>
+                                <Text className='text-slate-400 text-md'>{item.createdAt}</Text>
                             </View>
                             <View className='content-end ml-[35] justify-center'>
                                 <ButtonGlue onPress={navigateToMoleDetails}>
