4d228250c7bb2f950097f489c6eb235b9253b353
NashIvan-cmd <nashivanramos@gmail.com>
2025-04-27 00:32:24 +0800

    Wrapped on GluestackUi

--- melanoma/app/_layout.tsx
+++ melanoma/app/_layout.tsx
@@ -1,11 +1,15 @@
 import { Slot } from 'expo-router';
 import { SessionProvider } from '@/services/authContext';
 
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
 export default function Root() {
   // Set up the auth context and render our layout inside of it.
   return (
     <SessionProvider>
+      <GluestackUIProvider mode='light'>
       <Slot />
+      </GluestackUIProvider>
     </SessionProvider>
   );
 }
